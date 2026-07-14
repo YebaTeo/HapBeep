b244317f2822ece9b3ca4ef3b8ac28d0050e0420
malivee <muhalif7123@gmail.com>
2026-07-14 20:50:37 +0700

    add: add field color in cat3egory

--- HapBeep Watch App/Models/Category.swift
+++ HapBeep Watch App/Models/Category.swift
@@ -1,18 +1,21 @@
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 class Category {
     @Attribute(.unique) var name: String
     var severity: Int
+    var color: Color
     
     @Relationship(inverse: \Sound.category)
     var sounds: [Sound]
     
     
-    init(name: String, severity: Int) {
+    init(name: String, severity: Int, color: Color) {
         self.name = name
         self.severity = severity
+        self.color = color
         self.sounds = []
     }
 }
