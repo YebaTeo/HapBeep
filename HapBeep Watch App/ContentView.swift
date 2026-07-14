d701c5fcd11fcc2d615613c97a96232effdf75bf
yebateo <yebateo24@gmail.com>
2026-07-14 08:27:14 +0700

    Initial Commit

---
+++ HapBeep Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HapBeep Watch App
+//
+//  Created by Yeba Teo on 14/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HapBeep Watch App/HapBeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapBeepApp.swift
+//  HapBeep Watch App
+//
+//  Created by Yeba Teo on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HapBeep_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
