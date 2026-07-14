23a20328ceed231486420f985a6c70a3d5f568ec
malivee <muhalif7123@gmail.com>
2026-07-14 10:32:13 +0700

    modify some view

--- HapBeep Watch App/ContentView.swift
+++ HapBeep Watch App/ContentView.swift
@@ -2,23 +2,48 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @State private var isStartingDrivingMode: Bool = false
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome to HapBeep App!")
-                    .multilineTextAlignment(.center)
-            }
-            .padding()
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink {
-                        SettingsView()
-                    } label: {
-                        Image(systemName: "gear")
+            ZStack {
+                LinearGradient(colors: [.black, .primaryDarkBlue], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    if isStartingDrivingMode {
+                        
+                        Image(systemName: "car")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 34)
+                            .padding()
+                        Text("Driving Mode: off")
+                            .font(.title3.bold())
+                        
+                        
+                        Button {
+                            isStartingDrivingMode = !isStartingDrivingMode
+                        } label: {
+                            Label("", systemImage: "play.fill")
+                                .labelStyle(.iconOnly)
+                        }
+                    }
+                    ProgressView(value: 0)
+                }
+                .padding()
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        NavigationLink {
+                            SettingsView()
+                        } label: {
+                            Image(systemName: "gear")
+                        }
                     }
                 }
+                
             }
         }
+
     }
 }
 
